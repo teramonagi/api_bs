cfe7240860caf8925f5ba502c4ac98fd90dc0955
teramonagi <teramonagi@gmail.com>
2012-12-30 13:40:59 +0900

    README追加

--- api_bs.py
+++ api_bs.py
@@ -3,14 +3,14 @@ from flask import Flask, request
 from scipy.stats import norm
 import numpy as np
 
+HOST="127.0.0.1"
+PORT="5000"
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return """This is Black-Scholes API.
-        sample query
-        http://127.0.0.1:5000/bs_api?s0=100&k=100&T=5&r=0.1&vol=0.3            
-    """
+    return "Hello, world"
 
 @app.route("/bs_api")
 def bs_api():
@@ -27,5 +27,6 @@ def call_price_bs(s0, k, T, r, vol):
     d1 = (np.log(s0/k)+(r+0.5*(vol**2.0))*T) / (vol * np.sqrt(T))
     d2 = d1-vol*np.sqrt(T)
     return (s0*norm.cdf(d1) - k*np.exp(-r*T)*norm.cdf(d2))
+
 if(__name__ == '__main__'):
-    app.run()
+    app.run(host=HOST,port=int(PORT))
